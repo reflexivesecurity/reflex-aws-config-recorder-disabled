829e1523329a719a806ebd65ac5c8b10718baefe
rjulian <richard@rjulian.net>
2021-03-26 12:55:08 -0700

    Terraform formatting

--- terraform/cwe/cwe.tf
+++ terraform/cwe/cwe.tf
@@ -1,5 +1,5 @@
 module "cwe" {
-  source           = "git::https://github.com/reflexivesecurity/reflex-engine.git//modules/cwe?ref=v2.1.3"
+  source      = "git::https://github.com/reflexivesecurity/reflex-engine.git//modules/cwe?ref=v2.1.3"
   name        = "ConfigRecorderDisabled"
   description = "Event rule to catch disable or delete configuration recorder."
 
--- terraform/sqs_lambda/sqs_lambda.tf
+++ terraform/sqs_lambda/sqs_lambda.tf
@@ -1,14 +1,14 @@
 module "sqs_lambda" {
-  source           = "git::https://github.com/reflexivesecurity/reflex-engine.git//modules/sqs_lambda?ref=v2.1.3"
+  source                    = "git::https://github.com/reflexivesecurity/reflex-engine.git//modules/sqs_lambda?ref=v2.1.3"
   cloudwatch_event_rule_id  = var.cloudwatch_event_rule_id
   cloudwatch_event_rule_arn = var.cloudwatch_event_rule_arn
   function_name             = "ConfigRecorderDisabled"
   package_location          = var.package_location
   handler                   = "config_recorder_disabled.lambda_handler"
   lambda_runtime            = "python3.7"
-  environment_variable_map  = {
+  environment_variable_map = {
     SNS_TOPIC = var.sns_topic_arn,
-    
+
   }
   custom_lambda_policy = <<EOF
 {
